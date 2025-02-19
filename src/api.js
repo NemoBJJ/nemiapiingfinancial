cb78aa8933b60129431e5687a8db343f7e14c911
NemoBJJ <engnfaraujo@gmail.com>
2025-02-19 19:05:27 +0000

    FRONT E BACK HTTPS

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuração para utilizar o IP elástico com HTTPS
 const api = axios.create({
-    baseURL: 'https://3.217.55.187:8085/api', // Usando o IP elástico com HTTPS
+    baseURL: 'https://3.217.55.187:8443/api', // Usando o IP elástico com HTTPS e porta 8443
 });
 
 export default api;
