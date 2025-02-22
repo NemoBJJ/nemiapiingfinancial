2b839bc91f799028dcd9e584901ce80ccd2097d0
NemoBJJ <engnfaraujo@gmail.com>
2025-02-22 16:20:14 +0000

    HTTPS ultima se Deus quiser

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Configuração para utilizar o IP elástico com HTTP
 const api = axios.create({
-    baseURL: 'http://3.217.55.187:80/api', // Porta 80
+  baseURL: 'https://api1.neemindev.com/api',
 });
+
 
 export default api;
