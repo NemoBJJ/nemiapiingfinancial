4b5033eb482838302b598725e7bd1063a180dac3
NemoBJJ <engnfaraujo@gmail.com>
2026-07-29 12:49:57 -0300

    Migrando API Financeira do Render para Contabo

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const isDevelopment = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const isDevelopment =
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '127.0.0.1';
 
 const BASE_URL = isDevelopment
   ? 'http://localhost:8081/api'
-  : 'https://financialapiback.onrender.com/api';
+  : 'https://financial-container.neemindev.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
